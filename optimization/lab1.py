a7ace5949e083faa545bdaad64673a1a9996e432
Nastya <pototckaia@gmail.com>
2019-10-28 21:10:44 +1000

    Add first lab optimization

--- optimization/lab1.py
+++ optimization/lab1.py
@@ -11,6 +11,17 @@ def isPD(B):
         return False
 
 
+def rowToCol(v):
+	if len(v.shape) == 2:
+		return v.reshape((v.shape[1], 1))
+	else:
+		return v.reshape((v.shape[0], 1))
+
+
+def colToRow(v):
+	return v.reshape(v.shape[0])
+
+
 r'''
 	backtracking line search
 	- fix beta \in (0, 1) - B&T recommend (0.1, 0.8)
@@ -18,16 +29,16 @@ r'''
 		f(x - t * df(x)) > f(x) - t/2 * ||df(x)||^2_2 - Armijo-Goldstein condition
 		update t = beta * t
 '''
-def backtrackingLineSerarch(init_t, beta, x, f, df, max_iter=10):
-	cur_t = init_t
-	c = 1/2
-	for _ in range(1, max_iter):
-		a = f(x - cur_t * df(x))
-		b = f(x) - c * cur_t * la.norm(df(x), 2)**2
-		if a > b:
-			break
-		cur_t *= beta
-	return cur_t
+def backtrackingLineSerarch(f, df, x, p, min_t, c1, c2):
+	f_0 = f(x)
+	grad_f_0 = df(x) @ p.transpose()
+	t = 1
+	while t >= min_t:
+		f_t = f(x - t * p)
+		if f_t <= f_0 - t * c1 * grad_f_0:
+			return t
+		t *= c2
+	return min_t
 
 
 r'''
@@ -37,16 +48,16 @@ r'''
 	- exact line search
 		t = argmin(f(x - s * df(x)))
 '''
-def gradientDescent(f, df, x0, precision, lr = None, max_iter=30):
+def gradientDescent(f, df, x0, precision, lr = None, max_iter=100):
 	cur_x = x0
 	learning_rate = 1
 	dot = [x0]
 	res = {}
 	for _ in range(1, max_iter):
 		if lr is not None:
-			learning_rate = 1
+			learning_rate = lr
 		else:
-			learning_rate = backtrackingLineSerarch(learning_rate, 0.7, cur_x, f, df)
+			learning_rate = backtrackingLineSerarch(f, df, cur_x, df(cur_x), 0.001, 0.5, 0.1)
 		next_x = cur_x - learning_rate * df(cur_x)
 		if (la.norm(df(next_x), 2) <= precision): # || df(x^(k+1)) || = 0
 			cur_x = next_x
@@ -56,9 +67,9 @@ def gradientDescent(f, df, x0, precision, lr = None, max_iter=30):
 		dot.append(cur_x)
 		cur_x = next_x
 
-	res['x'] = cur_x.transpose()
+	res['x'] = cur_x
 	res['fun'] = f(cur_x)
-	res['dx'] = df(cur_x).transpose()
+	res['jac'] = df(cur_x)
 	res['iteration'] = len(dot)
 	return res
 
@@ -68,8 +79,8 @@ def newtonMethod(f, df, d2f, x0, precision, max_iter=1000):
 	dot = [x0]
 	res = {}
 	for _ in range(1, max_iter):
-		next_x = cur_x - la.inv(d2f(cur_x)) @ df(cur_x)
-		if (la.norm(df(next_x), 2) <= precision):
+		next_x = cur_x - df(cur_x) @ la.inv(d2f(cur_x))
+		if (la.norm(df(next_x), 2) <= precision): 
 			cur_x = next_x
 			res['success'] = True
 			break
@@ -77,78 +88,71 @@ def newtonMethod(f, df, d2f, x0, precision, max_iter=1000):
 		dot.append(cur_x)
 		cur_x = next_x
 	
-	res['x'] = cur_x.transpose()
+	res['x'] = cur_x
 	res['fun'] = f(cur_x)
-	res['dx'] = df(cur_x).transpose()
+	res['jac'] = df(cur_x)
 	res['iteration'] = len(dot)
 	return res
 
 n = 6
 A = make_spd_matrix(n)
-A = np.array([[ 0.41288544,  0.14402624,  0.08398111,  0.14910279,  0.19943268, -0.1850263 ],
- [ 0.14402624,  0.519214,    0.49199552,  0.67888757, -0.02225559, -0.20041139],
- [ 0.08398111,  0.49199552,  1.67634324,  2.25012679,  0.34441188, -1.10743227],
- [ 0.14910279,  0.67888757,  2.25012679,  3.85956323,  0.12809546, -1.90510422],
- [ 0.19943268, -0.02225559,  0.34441188,  0.12809546,  0.53617464, -0.10144008],
- [-0.1850263,  -0.20041139, -1.10743227, -1.90510422, -0.10144008,  1.44175826]])
-# print('is positive-definite matrix:', isPD(A))
-# print(A)
+A = np.array([[ 3.54575775,  1.40632856,  2.36437092, -1.01604035, -0.42511447, -0.25484018],
+ [ 1.40632856,  0.71820751,  1.1093031,  -0.35012359, -0.24096428, -0.10150292],
+ [ 2.36437092,  1.1093031,   2.24586614, -0.84271466, -0.22288024, -0.17881253],
+ [-1.01604035, -0.35012359, -0.84271466,  0.82865489,  0.15732203,  0.18745785],
+ [-0.42511447, -0.24096428, -0.22288024,  0.15732203,  0.39335921,  0.24514533],
+ [-0.25484018, -0.10150292, -0.17881253,  0.18745785,  0.24514533,  0.44956944]])
+print('is positive-definite matrix:', isPD(A))
+print(A)
 b = np.matrix('1; 2; 3; 4; 5; 6')
-# print(b)
+print(b)
 if A.shape[0] != A.shape[1]:
 	print('Матрица A должна быть квадратной')
 	exit(0)
 if b.shape[0] != n or b.shape[1] != 1:
-	print('Вектор-столбца b не правильно задан')
+	print('Вектор-столбец b не правильно задан')
 	exit(0)
 
 # 1/2 * (x^T * A * x) + b^T * x
 def f(x):
+	x = rowToCol(x)
 	return (0.5 * (x.transpose() @ A @ x)  + b.transpose() @ x)[0, 0]
+
 
 # 1/2 * (A + A^T) * x + b
 def df(x):
+	x = rowToCol(x)
 	aa = (A + A.transpose())
-	return 0.5 * aa @ x + 0.5 * b
+	res = 0.5 * aa @ x + 0.5 * b
+	return colToRow(res)
+
 
 # 1/2 * (A + A^T)
 def d2f(x):
 	return 0.5 * (A + A.transpose())
 
-x0 = np.zeros((n, 1))
 
-from scipy.misc import derivative
-def partial_derivative(func, var=0, point=[]):
-    args = point[:]
-    def wraps(x):
-        args[var] = x
-        return func(args)
-    return derivative(wraps, point[var], dx = 1e-10)
-def der(f, x):
-	res = []
-	for i in range(x.shape[0]):
-		res.append(partial_derivative(f, i, b))
-	return res
+x0 = np.zeros(n)
+print('xo', x0)
 
-print(df(b))
-for i in range(n):
-	print(partial_derivative(f, i, b))
-
-res = minimize(f, x0=x0)
+res = minimize(f, x0=x0, method='Newton-CG', 
+				jac=lambda x: np.ravel(df(x)), hess=d2f,
+				options={'xtol': 1e-5})
 print('Результат scipy.optimize.minimize')
 print('\t message: {}'.format(res['message']))
 print('\t success: {}'.format(res['success']))
 print('\t fun: {}'.format(res['fun']))
 print('\t x: {}'.format(res['x']))
+print('\t jac: {}'.format(res['jac']))
 print()
 
-ans = gradientDescent(f, df, x0, 0.005, 30)
+ans = gradientDescent(f, df, x0, 1e-6) # 0.3
 print('Результат gradientDescent')
 for key in ans:
 	print('\t {}: {}'.format(key, ans[key]))
 print()
 
-ans = newtonMethod(f, df, d2f, x0, 0.005)
+ans = newtonMethod(f, df, d2f, x0, 1e-6)
 print("Результат newtonMethod")
 for key in ans:
 	print('\t {}: {}'.format(key, ans[key]))
