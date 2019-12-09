b56a89759002608f1aa35af92d2ca53690e75131
pototckaia <pototckaia@gmail.com>
2019-12-10 06:09:32 +1000

    update tu.pl

--- prolog/tu.pl
+++ prolog/tu.pl
@@ -9,3 +9,23 @@
 
 :- if
 issister(X, Y):-female(X), parent(X, M, F), parent(Y, M, F).
+
+characters
+1. printing
+A, B, ...
+a, b, ...
+0, ..., 9
+*, -, /, \, ~, ^, <, >, ...
+2. no-printing
+blank space
+newline
+beeping sound
+
+constants
+1. numbers
+-17, 18, 1.01e2, 67, 1.01*10^-2
+2. atoms
+ 1. Type 1 -> letter + digit beging with lower case
+ 	ahi, dev,
+ 2. Type 2 -> sign *, -, +, /, \, 
+ 	math-pashan, 2018yac
