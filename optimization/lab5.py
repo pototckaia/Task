f515b0bdc3e487b42172fcf392b7476ab5c0ea73
pototckaia <pototckaia@gmail.com>
2019-12-24 09:55:24 +1000

    add lab5

--- optimization/lab5.py
+++ optimization/lab5.py
@@ -10,158 +10,141 @@ b неотрицательны
 
  Ищется оптимальноерешение  (симплекс-метод), 
  В двойственнойзадаче  ищется допустимое  и оптимальное рещения.
+
+
+table 
+базис  | B   | x_1 ...  x_k ... x_j ... x_n 
+ ...
+  x_t  | b_t |          a_tk   a_tj
+  ...
+  x_r  | b_r |          a_rk   a_rj
+  ...  | b_m |
+  ___  | ____|__________________________________
+  f(x) |  0  | -c_1 ...        -c_j
+
+
+1.Проверка базисного плана на оптимальность по условия
+если при каком-либо ДБР(допустимое базисное решение)
+в симплекс-таблице все коэфф. стоки f(x) не отрицательные
+то ДБР оптимально. В противном 2
+2. Переходи на новый базис
+Из числа небазисных переменных с отрицательным значениями 
+в последней строке выбирается перменная, вводимая в базис
+ - x_k, это переменная соответсвует наибольшая
+ по модулю отрицательная оценка
+ |c_k| = max |c_j|, c_j < 0
+столбец отвечающий переменной x_k называется главным 
+3. r - находим через
+b_r / a_rk = min_i (a_i/a_ik), a_ik >= 0
+сторока таблице ведущая 
+4.
+ 1.
+  b_k = b_r / a_rk
+  a_kj = a_rj / a_rk
+ 2.
+  a`_ij = a_ij - (a_rj * a_ik) / a_rk
+  b`_i = b_i - (b_r * a_ik) / a_rk
+  f`(x) = f(x) - (b_r * c_k) / a_rk
+  c`_j = c_j - (a_rj * c_k) / a_rk  
+
 '''
+
+
+
+
 import numpy as np
 import math
 
-def first_iteration(data): 
-    extended_factories = len(data['B'])
-    nullArr = []
-    for j in range(extended_factories):
-        nullArr.append(0)
-    matrixArr = []
-    for j in range(extended_factories):
-        resourses = []
-        for k in range(len(data['A'][j])):
-            resourses.append(int(data['A'][j][k]))
-        ones = np.zeros((3, 3), int)
-        np.fill_diagonal(ones, 1)
-        matrixArr.append([int(data['B'][j])] + resourses + ones[j].tolist())
-    relations = []
-    number_of_lead_column = data['C'].index(str(min(map(int,data['C']))))
-    lead_column_elements = []
-    for o in range(len(matrixArr)):
-        lead_column_elements.append(matrixArr[o][number_of_lead_column+1])
-    for l in range(len(lead_column_elements)):
-        try:
-            relations.append(int(int(data['B'][l])/lead_column_elements[l]))
-        except ZeroDivisionError:
-            relations.append(float('inf'))
-    y = []
-    for j in range(len(data['B'])):
-        y.append(str(j + len(data['C']) + 1))
-    new_data = 
-    	{'B': data['B'],
-    	 'C': [0] + list(map(int,d['C'])) + nullArr,
-    	 'A':matrixArr,
-    	 'Relations':relations,
-    	 'y':y
-    	 }
-    print('Первая матрица:')
-    print_table(new_data)
-    return new_data
 
-def iteration(data):
-    relations = []
-    number_of_lead_column = data['C'].index(min(data['C']))
-    lead_column_elements = []
-    for o in range(len(data['A'])):
-        lead_column_elements.append(data['A'][o][number_of_lead_column])
-    for l in range(len(lead_column_elements)):
-        try:
-            relations.append(float(float(data['B'][l])/lead_column_elements[l]))
-        except ZeroDivisionError:
-            relations.append(float('inf'))
-    number_of_lead_row = relations.index(min(relations))
-    lead_element = float(data['A'][number_of_lead_row][number_of_lead_column])
-    print('Разрешающий элемент:' + str(lead_element) + ' на позиции ' + str(number_of_lead_row) + ',' + str(number_of_lead_column) + '\n') 
-    print('Замена базиса: ' + data['y'][number_of_lead_row] + ' out, ' + str(number_of_lead_column) + '<-in\n')
-    data['y'][number_of_lead_row] = str(number_of_lead_column)
-    print('y = '+(',').join(data['y']))
-    print('Меняю матрицу\n')
- 
-    for i in range(len(data['C'])):
-        if i !=number_of_lead_column:
-            A=data['A'][number_of_lead_row][i]
-            B=data['C'][number_of_lead_column]
-            print('Элемент '+str(data['C'][i])+' из вектора С под номером '+str(i+1)+' -> ',end='')
-            data['C'][i]=data['C'][i]-((A*B)/lead_element)
-            print(data['C'][i])
-    #lead column c element
-    data['C'][number_of_lead_column]=0.0
-    #main elements
-    a_basic=data['A']
-    for i in range(a_basic.__len__()):
-        for j in range(a_basic[i].__len__()):
-            num = a_basic[i][j]
-            if i!=number_of_lead_row and j!=number_of_lead_column:
-                A=a_basic[i][number_of_lead_column]
-                B=a_basic[number_of_lead_row][j]
-                num_old=num
-                num=num-(A*B)/lead_element
-                print('Элемент '+str(num_old)+' на позиции '+str(i)+','+str(j)+' -> '+str(num))
-                data['A'][i][j]=num
-    data['A'][number_of_lead_row][number_of_lead_column]=1.0
-    print('Разрешающий элемент:'+str(lead_element)+' на позиции '+str(number_of_lead_row)+','+str(number_of_lead_column)+' -> 1 ')
- 
-    #lead row
-    for i in range(2,data['A'][number_of_lead_row].__len__()):
-        try:
-            print('Элемент '+str(data['A'][number_of_lead_row][i])+' на позиции '+str(number_of_lead_row)+','+str(i)+' -> ',end='')
-            data['A'][number_of_lead_row][i]=a_basic[number_of_lead_row][i]/lead_element
- 
-        except ZeroDivisionError:
-            data['A'][number_of_lead_row][i]=float('inf')
-        print(data['A'][number_of_lead_row][i])
-    #lead column
-    for j in range(data['A'].__len__()):
-        if(j!=number_of_lead_row):
-            print('Элемент '+str(data['A'][j][number_of_lead_column])+' на позиции '+str(j)+','+str(number_of_lead_column)+' -> 0')
-            data['A'][j][number_of_lead_column]=0
- 
-    print('Элемент '+str(data['A'][number_of_lead_row][0])+' на позиции '+str(number_of_lead_row)+','+str(0)+' -> ',end='')
-    #lead row first element
-    data['A'][number_of_lead_row][0]=float(a_basic[number_of_lead_row][0])/lead_element
-    print(data['A'][number_of_lead_row][0])
-    for k in range(data['B'].__len__()):
-        data['B'][k]=data['A'][k][0]
- 
- 
-    #relations
-    relations=[]
-    number_of_lead_column = data['C'].index(min(data['C']))
-    lead_column_elements=[]
-    for o in range(data['A'].__len__()):
-        lead_column_elements.append(data['A'][o][number_of_lead_column])
-    for l in range(lead_column_elements.__len__()):
-        try:
-            relations.append(float(float(data['B'][l])/lead_column_elements[l]))
-        except ZeroDivisionError:
-            relations.append(float('inf'))
-    data['Relations']=relations
-    return data
+def simplex(source, result):
+	m, n = source.shape
+	table = np.eye(m, n + m - 1)
+	basis = []
+
+	for i in range(0, table.shape[0]):
+		for j in range(0, table.shape[1]):
+			if j < n:
+				table[i, j] = source[i, j]
+			else:
+				table[i, j] = 0
+		if n + i < table.shape[1]:
+			table[i, n + i] = 1
+			basis.append(n + i)
+	n = table.shape[1]
+
+	mainCol, mainRow = 0, 0
+	while min(table[m - 1, 1:n]) < 0:
+		mainCol = 1 + np.argmin(table[m - 1, 1:n])
+		mainRow = findMainRow(m, m, table, mainCol)
+		if mainRow is None:
+			return None
+		basis[mainRow] = mainCol
+
+		new_table = np.eye(m, n)
+		for j in range(0, n):
+			new_table[mainRow, j] = table[mainRow, j] / table[mainRow, mainCol]
+		
+		for i in range(0, m):
+			if i == mainRow:
+				continue		
+			for j in range(0, n):
+				new_table[i, j] = table[i, j] - table[i, mainCol] * new_table[mainRow, j]
+		table = new_table
+
+	for i in range(0, len(result)):
+		if (basis.count(i + 1) > 0):
+			k = basis.index(i + 1)
+			result[i] = table[k, 0]
+		else:
+			result[i] = 0
+	return table
 
 
-def extract_result(data, max_numbers):
-    print("Максимальный результат  " + str(data["C"][0]))
-    print("При:")
-    
-    y_numbers_used = []
-    for i in range(len(data['y'])):
-        if int(data['y'][i]) <= len(data['y']):
-            y_numbers_used.append(int(data['y'][i]))
-            print("X" + str(data['y'][i])+" = "+ str(data['B'][i]))
-    print("Проверка:")
-    result = 0
-    check_str = ''
-    for i in y_numbers_used:
-        result += data['B'][data['y'].index(str(i))] * float(max_numbers[i-1]) * (-1)
-        check_str += str(data['B'][data['y'].index(str(i))]) + "*" + str(float(max_numbers[i-1]) * (-1)) + "+"
-    check_str = check_str[:-1]
-    print(check_str+'='+str(result))
+def findMainRow(m, n, table, mainCol):
+	cand = np.empty(shape=[0, 2])
+	for i in range(0, m - 1):
+		if (table[i, mainCol] != 0 and table[i, 0] / table[i, mainCol] > 0):
+			v = table[i, 0] / table[i, mainCol]
+			cand = np.append(cand, [[v, i]], axis=0)
+	if cand.shape[0] == 0:
+		return None
+	return int(cand[np.argmin(cand[:, 0]), 1])
 
-if __name__ == '__main__':
-    
-    d = {
-    	'A' : [['2', '3', '1'], 
-    			['4', '2', '2'], 
-    			['2', '1', '2']], 
-    	'C': ['5', '-2', '-3'], 
-    	'B': ['10', '12', '8']
-    	}
-    
-    n = first_iteration(d)
-    while min(n['C']) < 0:
-        n = iteration(n)
-            
-    extract_result(n, d['C'])
+
+'''
+f(x_1, x_2) = 6 * x_1 + 5 * x_2 -> max
+-3 * x_1 + 5 * x_2 <= 25
+-2 * x_1 + 5 * x_2 <= 30
+x_1 <= 10
+3 * x_1 - 8 * x_2 <= 6
+x_1, x_2 >= 0
+'''
+A = np.array([[-3.0, 5],
+	[-2.0, 5.0],
+	[1.0, 0.0],
+	[3.0, -8.0]])
+B = np.array([25.0, 30.0, 10.0, 6.0])
+C = np.array([-6.0, -5.0])
+
+s = np.eye(A.shape[0] + 1, A.shape[1] + 1)
+
+s[0:B.shape[0], 0] = B
+s[0][s.shape[1] - 1] = 0
+s[s.shape[0] - 1, 1:s.shape[1]] = C
+s[0:s.shape[0] - 1, 1:s.shape[1] + 1] = A
+
+result = np.zeros(2)
+
+table = simplex(s, result)
+print(table)
+print(result)
+
+s = np.array([[240.0, 2.0, 3.0, -6.0],
+ [200.0, 4.0, 2.0, -4.0],
+ [160.0, 4.0, 6.0, -8.0],
+ [0.0, -4.0, -5.0, -4.0]])
+result = np.zeros(3)
+
+table = simplex(s, result)
+print(table)
+print(result)
